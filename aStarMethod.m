7248bafa30b810f3614b372f8254442a5ae97cad
a-person14 <116467877+a-person14@users.noreply.github.com>
2024-07-28 20:01:12 -0700

    Update aStarMethod.m

--- aStarMethod.m
+++ aStarMethod.m
@@ -83,14 +83,15 @@ ylim([ymin, ymax]);
 title('COSMOS 24 Cluster 11 Maze');
 
 
-
+%%
+clc;
 
 Parents = zeros(100,100, 2);
 Gcosts = zeros(100,100);
 Hcosts = zeros(100,100); %(gcost, hcost)
 Fcosts = zeros(100,100);
 OpenNodes = []; %coordinates of the nodes
-ClosedNodes = {};
+ClosedNodes = [];
 StartNode = [0,-10];
 EndNode = [0,10];
 
@@ -120,20 +121,20 @@ if neighbour is not in OPEN
 add neighbour to OPEN
 %}
 
-function path = findPath(startNode, endNode, OpenNodes, ClosedNodes, Gcosts, Fcosts, Parents) %input coordinates 
-    OpenNodes = [OpenNodes; startNode];
+function path = findPath(startNode, endNode, OpenNodes, ClosedNodes, Gcosts, Fcosts, Parents, obstacles) %input coordinates 
+    OpenNodes = [OpenNodes; startNode]
     while ~isempty(OpenNodes)
         CurrNode = [OpenNodes(1, 1), OpenNodes(1, 2)] %initializing current to the start node
         for i = 1:size(OpenNodes, 1) %taking the one with the lowest cost
             if Fcosts(OpenNodes(i, 1), OpenNodes(i,2)) < Fcosts(CurrNode) 
             CurrNode = [OpenNodes(i, 1), OpenNodes(i, 2)]
-            elseif  Fcosts(OpenNodes(i, 1), OpenNodes(i,2)) == Fcosts(CurrNode) & h_cost(OpenNodes(i, 1), OpenNodes(i,2)) < h_cost(CurrNode)
+            elseif Fcosts(OpenNodes(i, 1), OpenNodes(i,2)) == Fcosts(OpenNodes(1, 1), OpenNodes(1, 2)) && h_cost([OpenNodes(i, 1), OpenNodes(i,2)]) < h_cost([OpenNodes(1, 1), OpenNodes(1, 2)])
             CurrNode = [OpenNodes(i, 1), OpenNodes(i, 2)]
             end
         end
 
         OpenNodes(1, :) = [];
-        ClosedNodes = {ClosedNodes; CurrNode};
+        ClosedNodes = [ClosedNodes; CurrNode];
 
         if CurrNode == endNode
             path = retracePath(startNode, endNode, Parents)
@@ -141,15 +142,23 @@ function path = findPath(startNode, endNode, OpenNodes, ClosedNodes, Gcosts, Fco
             return
         end
         
-        for neighbor = getNeighbors(CurrNode)
-            if detection(matrixToCoords(neighbor)) | ismember(neighbor, ClosedNodes)
+        listNeigh = getNeighbors(CurrNode)
+
+        for indx = 1:size(listNeigh, 1)
+            neighbor = listNeigh(indx, :)
+            coords = matrixToCoords(neighbor)
+            plot(coords, '.' ,'MarkerSize',12)
+            if detection(coords(1), coords(2), obstacles) | ismember(neighbor, ClosedNodes)
                continue 
             end
+            
 
-            neighborPathCost = Gcosts(CurrNode)+getDistance(CurrNode, neighbor)
+            neighborPathCost = Gcosts(CurrNode(1), CurrNode(2))+getDistance(CurrNode, neighbor)
 
             if neighborPathCost<getDistance(neighbor, endNode) | ismember(neighbor, OpenNodes) == false
-                Fcosts(neighbor) = neighborPathCost + getDistance(neighbor, endNode);
+                coords = matrixToCoords(neighbor)
+                Gcosts(neighbor(1), neighbor(2)) = Gcosts(CurrNode(1), CurrNode(2))+getDistance(CurrNode, neighbor);
+                Fcosts(neighbor(1), neighbor(2)) = neighborPathCost + getDistance(neighbor, endNode);
                 Parents(neighbor, 1) = CurrNode(1);
                 Parents(neighbor, 2) = CurrNode(2);
             end
@@ -178,8 +187,8 @@ function neighbors = getNeighbors(CurrNode)
             if x == 0 && y==0
                 continue
             end
-            if 0<=CurrNode(1)+x && CurrNode(1)+x<=100 && 0<=CurrNode(2)+y && CurrNode(2)+y<=100 %check neighbors
-                neighbors = [neighbors; CurrNode+[x,y]];
+            if 1<=CurrNode(1)+x && CurrNode(1)+x<=101 && 1<=CurrNode(2)+y && CurrNode(2)+y<=101 %check neighbors
+                neighbors = [neighbors; [CurrNode(1)+x,CurrNode(2)+y]];
             end
         end
     end
@@ -187,7 +196,7 @@ end
 
 function distance = getDistance(NodeA, NodeB)
     xDist = abs(NodeB(1) - NodeA(1));
-    yDist = abd(NodeA(1) - NodeB(1));
+    yDist = abs(NodeA(1) - NodeB(1));
 
     if xDist > yDist
         distance = 14*yDist+10*(xDist-yDist);
@@ -225,27 +234,13 @@ function in_obstacle = detection(x, y, obstacles) %use in neightbors
 end
 
 function xy = matrixToCoords(node)
-    xMatrix = node(1); yMatrix = node(2);
-    xCoord = (yMatrix-50)/5;
-    if xMatrix<=50 
-        yCoord = (xMatrix-50)/5;
-    elseif xMatrix>=50 && yMatrix<=50 
-        yCoord = (50-xMatrix)/5;
-    end
-    xy = [xCoord, yCoord];
+    xy = [(node(1)-51)/5, (node(2)-51)/5];
 end
 
 function xy = coordsToMatrix(node)
-    xCoord = node(1); yCoord = node(2);
-    yMatrix = 50 + xCoord*5;
-    if yCoord >= 0 
-        xMatrix = 50 + yCoord*5;
-    elseif yCoord <= 0
-        xMatrix = 50 - yCoord*5;
-    end
-    xy = [xMatrix, yMatrix];
+    xy = [node(1)*5+51, node(2)*5+51];
 end
 
 %logic
 %set startNode to open
-findPath([coordsToMatrix(StartNode)], [coordsToMatrix(EndNode)], OpenNodes, ClosedNodes, Gcosts, Fcosts, Parents)
+findPath([coordsToMatrix(StartNode)], [coordsToMatrix(EndNode)], OpenNodes, ClosedNodes, Gcosts, Fcosts, Parents, obstacles)
