b0a3c785962ea6f9aaedbb98b96cba47e0e7a2f4
a-person14 <116467877+a-person14@users.noreply.github.com>
2024-07-28 18:29:00 -0700

    Update aStarMethod.m

--- aStarMethod.m
+++ aStarMethod.m
@@ -1,8 +1,95 @@
-Parents = zeros(100,100);
+clc;
+clear;
+%close all;
+clf
+
+
+% Define maze boundaries
+xmin = -10;
+xmax = 10;
+ymin = -10;
+ymax = 10;
+
+% Define obstacles as inequalities in the form x <= f(y)
+obstacles = {
+%    @(x,y) y <= x.^2 - 5;                 % Example: parabolic obstacle
+%    @(x,y) y >= sin(x) - 3 && y <= 5;     % Example: sine wave obstacle
+    @(x,y) (x >= -8.5 && x <= 7.5) && (y >= -8 && y <= -7.5);  % Example: rectangular obstacle
+    @(x,y) (x >= 1 && x <= 1.5) && (y >= -10 && y <= -8);
+    @(x,y) (x >= 7 && x <= 7.5) && (y >= -7.5 && y <= -5.5);
+    @(x,y) (x >= -3 && x <= -2.5) && (y >= -7.5 && y <= -5.5);
+    @(x,y) (x >= -3 && x <= 5) && (y >= -5.5 && y <= -5);
+    @(x,y) (x >= -10 && x <= -8) && (y >= -5.5 && y <= -5);
+    @(x,y) (x >= -10 && x <= -5) && (y >= -5.5 && y <= -5);
+    @(x,y) (x >= -5.5 && x <= -5) && (y >= -5 && y <= -2);
+    @(x,y) (x >= -10 && x <= -3) && (y >= 0.5 && y <= 1);
+    @(x,y) (x >= -3 && x <= -2.5) && (y >= -2 && y <= 1);
+    @(x,y) (x >= -3 && x <= 7.5) && (y >= -2.5 && y <= -2);
+    @(x,y) (x >= 0 && x <= 7.5) && (y >= 0.5 && y <= 1);
+    @(x,y) (x >= 0 && x <= 0.5) && (y >= 1 && y <= 4);
+    @(x,y) (x >= -3 && x <= 10) && (y >= 3.5 && y <= 4);
+    @(x,y) (x >= -10 && x <= -6) && (y >= 3.5 && y <= 4);
+    @(x,y) (x >= -7.5 && x <= 7.5) && (y >= 7.5 && y <= 8);
+    @(x,y) (x >= -1.5 && x <= -1) && (y >= 8 && y <= 10);
+    @(x,y) (x >= 7 && x <= 7.5) && (y >= 6 && y <= 7.5);
+    @(x,y) (x >= 4.5 && x <= 5) && (y >= 4 && y <= 6);
+    @(x,y) (x >= -3 && x <= 1) && (y >= 5.5 && y <= 6);
+    @(x,y) (x >= -8 && x <= -7.5) && (y >= 6 && y <= 8);
+    @(x,y) (x >= -8 && x <= -2) && (y >= 5.5 && y <= 6);
+    @(x,y) x <= -10; 
+    @(x,y) x >= 10; 
+    @(x,y) y <= -10; 
+    @(x,y) y >= 10; 
+    
+};
+
+% Define start and end points
+start_point = [0, -10];
+end_point = [0, 10];
+
+% Visualization parameters
+resolution = 0.1;  % Resolution for plotting obstacles
+marker_size = 10;
+
+% Plot maze boundaries
+figure(1);
+hold on;
+plot([-1, xmin, xmin, -1], [ymax, ymax, ymin, ymin], 'k-', 'LineWidth', 2);
+plot([1, xmax, xmax, 1], [ymin, ymin, ymax, ymax], 'k-', 'LineWidth', 2);
+%plot([xmin, -1, xmax, xmax, xmin, xmin], [ymin, ymin, ymax, ymax, ymin], 'k-', 'LineWidth', 2);
+
+% Plot obstacles
+x_range = xmin:resolution:xmax;
+y_range = ymin:resolution:ymax;
+for i = 1:length(obstacles)
+    for j = 1:length(x_range)
+        for k = 1:length(y_range)
+            if obstacles{i}(x_range(j), y_range(k)) && x_range(j)>-10 && x_range(j)<10 && y_range(k)>-10 && y_range(k)<10
+                plot(x_range(j), y_range(k), 'r.', 'MarkerSize', 10);
+            end
+        end
+    end
+end
+
+% Plot start and end points
+plot(start_point(1), start_point(2), 'go', 'MarkerSize', marker_size, 'LineWidth', 2);  % Green circle for start
+plot(end_point(1), end_point(2), 'ro', 'MarkerSize', marker_size, 'LineWidth', 2);  % Red circle for end
+
+% Adjust axes and grid
+axis equal;
+grid on;
+xlim([xmin, xmax]);
+ylim([ymin, ymax]);
+title('COSMOS 24 Cluster 11 Maze');
+
+
+
+
+Parents = zeros(100,100, 2);
 Gcosts = zeros(100,100);
 Hcosts = zeros(100,100); %(gcost, hcost)
-Fcost = zeros(100,100);
-OpenNodes = []; %(fcost,hcost), pos
+Fcosts = zeros(100,100);
+OpenNodes = []; %coordinates of the nodes
 ClosedNodes = {};
 StartNode = [0,-10];
 EndNode = [0,10];
@@ -23,8 +110,8 @@ if current is the target node //path has been found ***
 return ***
 
 foreach neighbour of the current node *** 
-if neighbour is not traversable or neighbour is in CLOSED
-skip to the next neighbour
+if neighbour is not traversable or neighbour is in CLOSED ***
+skip to the next neighbour ***
 
 if new path to neighbour is shorter OR neighbour is not in OPEN
 set f_cost of neighbour
@@ -33,34 +120,56 @@ if neighbour is not in OPEN
 add neighbour to OPEN
 %}
 
-function findPath(startNode, endNode, OpenNodes, ClosedNodes) %input coordinates 
-    OpenNodes = [OpenNodes, startNode];
-    Costs(startNode) = 
-    while ~isempty(OpenSet)
-        CurrNode = OpenNodes(1, :); %initializing current to the start node
-        for i = 1:length(OpenNodes) %taking the one with the lowerst cost
-            if f_cost(OpenNodes(i, :))<f_cost(CurrNode) || f_cost(OpenNodes(i, :)) == f_cost(CurrNode) && h_cost(OpenNodes(i, :)) < h_cost(CurrNode)
-            CurrNode = OpenNodes(i, :);
+function path = findPath(startNode, endNode, OpenNodes, ClosedNodes, Gcosts, Fcosts, Parents) %input coordinates 
+    OpenNodes = [OpenNodes; startNode];
+    while ~isempty(OpenNodes)
+        CurrNode = [OpenNodes(1, 1), OpenNodes(1, 2)] %initializing current to the start node
+        for i = 1:size(OpenNodes, 1) %taking the one with the lowest cost
+            if Fcosts(OpenNodes(i, 1), OpenNodes(i,2)) < Fcosts(CurrNode) 
+            CurrNode = [OpenNodes(i, 1), OpenNodes(i, 2)]
+            elseif  Fcosts(OpenNodes(i, 1), OpenNodes(i,2)) == Fcosts(CurrNode) & h_cost(OpenNodes(i, 1), OpenNodes(i,2)) < h_cost(CurrNode)
+            CurrNode = [OpenNodes(i, 1), OpenNodes(i, 2)]
             end
         end
+
         OpenNodes(1, :) = [];
-        ClosedNodes = {ClosedNodes, CurrNode};
+        ClosedNodes = {ClosedNodes; CurrNode};
 
         if CurrNode == endNode
+            path = retracePath(startNode, endNode, Parents)
+            plot(path)
             return
         end
         
         for neighbor = getNeighbors(CurrNode)
-            if detection(matrixToCoords(neighbor)) | ~ismember(neighbor, ClosedNodes)
+            if detection(matrixToCoords(neighbor)) | ismember(neighbor, ClosedNodes)
                continue 
             end
 
-            neighborGcost = Gcosts
-        end
+            neighborPathCost = Gcosts(CurrNode)+getDistance(CurrNode, neighbor)
 
+            if neighborPathCost<getDistance(neighbor, endNode) | ismember(neighbor, OpenNodes) == false
+                Fcosts(neighbor) = neighborPathCost + getDistance(neighbor, endNode);
+                Parents(neighbor, 1) = CurrNode(1);
+                Parents(neighbor, 2) = CurrNode(2);
+            end
+
+            if ~ismember(neighbor, OpenNodes)
+                OpenNodes = [OpenNodes; neighbor]
+            end
+        end
     end
 end
 %if ~detection(matrixToCoords(CurrNode+[x,y])) 
+
+function Path = retracePath(startNode, endNode, Parents)
+    currentNode = startNode;
+    Path = [];
+
+    while currentNode ~= endNode
+        Path = [path, matriToCoords(Parents(startNode))];
+    end
+end
 
 function neighbors = getNeighbors(CurrNode)
     neighbors = [];
@@ -107,17 +216,15 @@ function in_obstacle = detection(x, y, obstacles) %use in neightbors
         in_obstacle = false;
         return;
     end
-    for i = 1:length(obstacles)
-        if obstacles{i}(x, y)
+    for a = 1:length(obstacles)
+        if obstacles{a}(x, y)
             in_obstacle = true;
             return; 
         end
     end
 end
 
-
-
-function [xCoord, yCoord] = matrixToCoords(node)
+function xy = matrixToCoords(node)
     xMatrix = node(1); yMatrix = node(2);
     xCoord = (yMatrix-50)/5;
     if xMatrix<=50 
@@ -125,9 +232,10 @@ function [xCoord, yCoord] = matrixToCoords(node)
     elseif xMatrix>=50 && yMatrix<=50 
         yCoord = (50-xMatrix)/5;
     end
+    xy = [xCoord, yCoord];
 end
 
-function [xMatrix, yMatrix] = coordsToMatrix(node)
+function xy = coordsToMatrix(node)
     xCoord = node(1); yCoord = node(2);
     yMatrix = 50 + xCoord*5;
     if yCoord >= 0 
@@ -135,19 +243,9 @@ function [xMatrix, yMatrix] = coordsToMatrix(node)
     elseif yCoord <= 0
         xMatrix = 50 - yCoord*5;
     end
+    xy = [xMatrix, yMatrix];
 end
 
 %logic
 %set startNode to open
-findPath([coordsToMatrix(StartNode)], [coordsToMatrix(EndNode)], OpenNodes, ClosedNodes)
-
-
-
-
-
-
-
-
-
-
-
+findPath([coordsToMatrix(StartNode)], [coordsToMatrix(EndNode)], OpenNodes, ClosedNodes, Gcosts, Fcosts, Parents)
