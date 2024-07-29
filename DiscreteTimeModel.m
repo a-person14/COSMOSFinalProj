824704103723edb8edba8947271102b75ede7d6f
jdeguzman11 <138526393+jdeguzman11@users.noreply.github.com>
2024-07-28 17:58:31 -0700

    Update DiscreteTimeModel.m

--- DiscreteTimeModel.m
+++ DiscreteTimeModel.m
@@ -1,32 +1,32 @@
-%initial conditions
+% initial conditions
 x0 = 0; %init xpos in meters
 y0 = 0; %init ypos
 theta0 = 0;
 v0 = 0;
 
-%parameters
+% parameters
 h = .01; %step size
 
-%init theta array
+% init theta array
 theta = [theta0];
 
-%Omega array
+% Omega array
 omega = [pi/2, pi/2, pi/2, pi/2, pi/2, pi/2];
 
-%velocities array
+% velocities array
 xvelocities = [v0, 1, 2, 1, 1, 1];
 yvelocities = [v0, 1, 1, 1, 1, 1];
 
-%positions array
+% positions array
 x=[x0];
 y=[y0];
 
-%creating the array of theta
+% creating the array of theta
 for i = 1:6
     theta(i+1) = theta(i) + h*omega(i);
 end
 
-%creating the array of x,y positions
+% creating the array of x,y positions
 for i = 1:6 
     x(i+1) = x(i)+h*xvelocities(i)*cos(theta(i)); 
     y(i+1) = y(i)+h*yvelocities(i)*sin(theta(i));
